2b6213679c1a00bbc94a84c45968ae9c58479cb2
Your Name <you@example.com>
2021-08-29 13:03:39 +0530

    Updates

---
+++ precache-manifest.6598166636348e28c2ac8973383699ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a07f6b061b4a0fb6f2de0b108f2b8666",
+    "url": "/react_project_todolist/index.html"
+  },
+  {
+    "revision": "1d5a164cf62d26f47951",
+    "url": "/react_project_todolist/static/css/main.bf14a96d.chunk.css"
+  },
+  {
+    "revision": "8e49ff261b0b40148c6b",
+    "url": "/react_project_todolist/static/js/2.c6b53d6d.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/react_project_todolist/static/js/2.c6b53d6d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d5a164cf62d26f47951",
+    "url": "/react_project_todolist/static/js/main.44e6735d.chunk.js"
+  },
+  {
+    "revision": "6b56fb65bc53f65b9535",
+    "url": "/react_project_todolist/static/js/runtime-main.6b9cdac5.js"
+  }
+]);
